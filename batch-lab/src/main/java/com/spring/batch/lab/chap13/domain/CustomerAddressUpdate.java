308b8367c25ba380c33e4f5cfa388a29120d44b3
BangKiHyun <rlrlvh@naver.com>
2021-10-10 17:58:28 +0900

    Update ItemReader 테스트

---
+++ batch-lab/src/main/java/com/spring/batch/lab/chap13/domain/CustomerAddressUpdate.java
@@ -0,0 +1,28 @@
+package com.spring.batch.lab.chap13.domain;
+
+import com.spring.batch.lab.chap13.CustomerUpdate;
+import lombok.Getter;
+import org.springframework.util.StringUtils;
+
+@Getter
+public class CustomerAddressUpdate extends CustomerUpdate {
+
+    private final String address1;
+
+    private final String address2;
+
+    private final String city;
+
+    private final String state;
+
+    private final String postalCode;
+
+    public CustomerAddressUpdate(long customerId, String address1, String address2, String city, String state, String postalCode) {
+        super(customerId);
+        this.address1 = StringUtils.hasText(address1) ? address1 : null;
+        this.address2 = StringUtils.hasText(address2) ? address2 : null;
+        this.city = StringUtils.hasText(city) ? city : null;
+        this.state = StringUtils.hasText(state) ? state : null;
+        this.postalCode = StringUtils.hasText(postalCode) ? postalCode : null;
+    }
+}
---
+++ batch-lab/src/main/java/com/spring/batch/lab/chap13/domain/CustomerNameUpdate.java
@@ -0,0 +1,20 @@
+package com.spring.batch.lab.chap13.domain;
+
+import com.spring.batch.lab.chap13.CustomerUpdate;
+import org.springframework.util.StringUtils;
+
+public class CustomerNameUpdate extends CustomerUpdate {
+
+    private final String firstName;
+
+    private final String middleName;
+
+    private final String lastName;
+
+    public CustomerNameUpdate(long customerId, String firstName, String middleName, String lastName) {
+        super(customerId);
+        this.firstName = StringUtils.hasText(firstName) ? firstName : null;
+        this.middleName = StringUtils.hasText(middleName) ? middleName : null;
+        this.lastName = StringUtils.hasText(lastName) ? lastName : null;
+    }
+}
