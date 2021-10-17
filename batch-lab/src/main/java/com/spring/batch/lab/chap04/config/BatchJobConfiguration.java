f72cb8f55d2f50c5104b3307ecb78ecef368c4ba
BangKiHyun <rlrlvh@naver.com>
2021-10-17 20:58:50 +0900

    Update Job과 Step의 ExecutionContext 조작 방법

---
+++ batch-lab/src/main/java/com/spring/batch/lab/chap04/config/BatchJobConfiguration.java
@@ -0,0 +1,60 @@
+package com.spring.batch.lab.chap04.config;
+
+import com.spring.batch.lab.chap04.tasklet.HelloTasklet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+@RequiredArgsConstructor
+public class BatchJobConfiguration {
+
+    private static final String JOB_NAME = "chap4_job";
+    private static final String STEP_NAME = "-chap4_step";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean(name = JOB_NAME)
+    public Job job() {
+        return this.jobBuilderFactory.get(JOB_NAME)
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean(name = "one" + STEP_NAME)
+    public Step step1() {
+        return this.stepBuilderFactory.get("one" + STEP_NAME)
+                .tasklet(new HelloTasklet())
+                .listener(promotionListener())
+                .build();
+    }
+
+    @Bean(name = "two" + STEP_NAME)
+    public Step step2() {
+        return this.stepBuilderFactory.get("two" + STEP_NAME)
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println("Good Bye");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    // step의 ExecutionContext에서 "name"키를 찾으면 Job의 ExecutionContext에 복사 (Step -> Job ExecutionContext로 승격)
+    @Bean
+    public StepExecutionListener promotionListener() {
+        ExecutionContextPromotionListener listener = new ExecutionContextPromotionListener();
+        listener.setKeys(new String[]{"name"});
+        return listener;
+    }
+}
